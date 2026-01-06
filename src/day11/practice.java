8aa20d6495356a072083269861fc1a718b75124a
divyaapalanivel <divyaapalanivel@gmail.com>
2026-01-06 23:54:43 +0530

    initial commit

--- src/day11/practice.java
+++ src/day11/practice.java
@@ -12,21 +12,39 @@ class practice implements Comparable<practice> {
         this.name = name;
     }
 
-    // Natural ordering based on marks
+    
     public int compareTo(practice p) {
-        return this.marks - p.marks;  // ascending order
+        return this.marks - p.marks; 
     }
 
     public static void main(String[] args) {
 
-        ArrayList<practice> list = new ArrayList<>();
+        
+        // Comparison (simple if-else)
+        
+        int a = 50;
+        int b = 75;
 
+        System.out.println("=== Comparison Example ===");
+        if (a > b) {
+            System.out.println(a + " is greater than " + b);
+        } else if (a < b) {
+            System.out.println(b + " is greater than " + a);
+        } else {
+            System.out.println(a + " and " + b + " are equal");
+        }
+
+        
+        // Comparable (sorting objects)
+        
+        ArrayList<practice> list = new ArrayList<>();
         list.add(new practice(85, "Divya"));
         list.add(new practice(92, "Arun"));
         list.add(new practice(78, "Ajith"));
 
         Collections.sort(list);
 
+        System.out.println("\n=== Comparable Example (Sorted by marks) ===");
         for (practice p : list) {
             System.out.println(p.marks + " " + p.name);
         }
